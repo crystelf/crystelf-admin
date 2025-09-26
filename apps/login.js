edad447c6ecc6b73ba6e0ea000bbcc8548807fd7
Jerryplusy <Jerryplusy@outlook.com>
2025-09-26 17:20:03 +0800

    feat:部分onebot11登录实现

---
+++ apps/login.js
@@ -0,0 +1,11 @@
+export default class LoginService extends plugin {
+  constructor() {
+    super({
+      name: 'Onebot登录相关服务',
+      dsc: '方便操作',
+      rule: [],
+    });
+  }
+
+  async test(e) {}
+}
